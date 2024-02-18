27a879ef32692feccbd18a23a078d4c511fc1d51
Randy Birdsey <birdseyrk@gmail.com>
2024-02-18 14:03:07 -0700

    Initial Delivery

--- taylorui/src/app/app.component.ts
+++ taylorui/src/app/app.component.ts
@@ -1,8 +1,21 @@
+// Angular Widgets
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// PrimeNG Widgets
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { PanelModule } from 'primeng/panel';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { ToastModule } from 'primeng/toast';
+import { TabViewModule } from 'primeng/tabview';
+import { TableModule } from 'primeng/table';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
+// Local Services
 import { ElevationService } from './elevation.service';
 
+// Local Widgets
 import { HomeComponent } from './home/home.component';
 import { ElevationComponent } from './elevation/elevation.component';
 
@@ -10,7 +23,21 @@ import { ElevationComponent } from './elevation/elevation.component';
   selector: 'app-root',
   standalone: true,
   imports: [
+
+    // Angular Widgets
     RouterOutlet,
+
+    // PrimeNG widgets
+    ButtonModule,
+    InputTextareaModule,
+    MenuModule,
+    PanelModule,
+    ScrollPanelModule,
+    TableModule,
+    TabViewModule,
+    ToastModule,
+
+    //Local Widgets
     HomeComponent,
     ElevationComponent],
   templateUrl: './app.component.html',
