a6b6539b13060601da6ca6aa181e7f4c9bad008a
Randy Birdsey <birdseyrk@gmail.com>
2024-02-18 11:08:04 -0700

    Initial Delivery

---
+++ taylorui/src/app/elevation.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import * as elevation from '../../assets/taylorelevationtable.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ElevationService {
+
+  elevationData: any = elevation;
+  elevation = 0.0;
+  acrefeet = 70000.0;
+
+  constructor() { } 
+  
+  getElevation() {
+    console.log("----------ElevationComponent - getElevation [" + this.acrefeet + "] ----------");
+
+  }
+}
