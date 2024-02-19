9d9f869f12112f530f3b1152f3eaec15fa053d1b
Randy Birdsey <birdseyrk@gmail.com>
2024-02-18 18:59:11 -0700

    elevation

--- taylorui/src/app/elevation.service.ts
+++ taylorui/src/app/elevation.service.ts
@@ -12,10 +12,47 @@ export class ElevationService {
 
   constructor() { }
   
-  getElevation() {
-    console.log("----------ElevationComponent - getElevation [" + this.acrefeet + "] ----------");
-    console.log(this.elevationData.elevationTable)
+  getElevation(acreFeet: number): number {
+
+    let tabIndex = 0;
+    let decIndex = 0;
+    let myDecimal = 0.0;
+
+    // console.log("----------ElevationComponent - getElevation [" + this.acrefeet + "] ----------");
+    // console.log(this.elevationData.elevationTable)
+    // console.log(this.elevationData.elevationTable.length);
+
+
+    this.elevationData.elevationTable.forEach(function (row:any) {
+      //console.log(row.low);
+      if (row.low < acreFeet ) {
+        tabIndex++;
+      } else {
+        return;
+      }
+    });
+
+    this.elevation = this.elevationData.elevationTable[(tabIndex-1)].elevation;
+
+    this.elevationData.elevationTable[(tabIndex-1)].acrefeet.forEach(function (row:any) {
+      //console.log(row);
+
+      if (row < acreFeet ) {
+        decIndex++;
+      } else {
+        return;
+      }
+    });
+
+    myDecimal = (acreFeet - this.elevationData.elevationTable[(tabIndex-1)].acrefeet[(decIndex-1)]) / (this.elevationData.elevationTable[(tabIndex-1)].acrefeet[(decIndex)] - this.elevationData.elevationTable[(tabIndex-1)].acrefeet[(decIndex-1)]) / 10;
+
+    //console.log(myDecimal.toFixed(2));
+
+    this.elevation = this.elevationData.elevationTable[(tabIndex-1)].elevation + (decIndex-1) * 0.1 + Number(myDecimal.toFixed(2));
+    //console.log(this.elevationData.elevationTable[(tabIndex-1)]);
+
+    return this.elevation;
 
   }
-  
+
 }
--- taylorui/src/app/elevation/elevation.component.ts
+++ taylorui/src/app/elevation/elevation.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { InputNumberInputEvent } from 'primeng/inputnumber';
 
+import { ElevationService } from '../elevation.service';
+
 @Component({
   selector: 'app-elevation',
   templateUrl: './elevation.component.html',
@@ -8,11 +10,17 @@ import { InputNumberInputEvent } from 'primeng/inputnumber';
 })
 export class ElevationComponent {
 
-  acrefeet = 70000.0;
+  constructor( 
+    public elevationService: ElevationService) { }
+
+  acrefeet   = 70000.0;
+  elevation = 0.0;
+  
 
   getElevation(event: MouseEvent) {
     console.log("getElevation " + this.acrefeet);
-    //console.log(event.valueOf);
+    console.log(this.elevationService.getElevation( this.acrefeet));
+    this.elevation = this.elevationService.getElevation( this.acrefeet);
   }
 
 }
