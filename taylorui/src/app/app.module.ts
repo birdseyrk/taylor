817ab52b7fbec3d5fe07458d8575f7a3de0b93bd
Randy Birdsey <birdseyrk@gmail.com>
2024-02-19 17:08:00 -0700

    Initial Delivery

--- taylorui/src/app/app.module.ts
+++ taylorui/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { FormsModule } from '@angular/forms';
 
 //NG Prime Widgets
-
+//import { PrimeNGConfig } from 'primeng/api';
 
 import { ButtonModule } from 'primeng/button';
+//import { CalendarModule } from 'primeng/calendar';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { InputTextModule } from 'primeng/inputtext';
@@ -16,6 +17,7 @@ import { PanelModule } from 'primeng/panel';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
 import { ToastModule } from 'primeng/toast';
 import { TabViewModule } from 'primeng/tabview';
+
 import { TableModule } from 'primeng/table';
 
 
@@ -36,11 +38,13 @@ import { ElevationComponent } from './elevation/elevation.component';
 
     // NG Prime Widgets
     ButtonModule,
+    //CalendarModule,
     InputNumberModule,
     InputTextareaModule,
     InputTextModule,
     MenuModule,
     PanelModule,
+    //PrimeNGConfig,
     ScrollPanelModule,
     TableModule,
     TabViewModule,
